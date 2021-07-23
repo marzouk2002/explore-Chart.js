4da77ad2b4aff57d21259523b33f325fc51fd43e
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-07-23 17:27:59 +0100

    first tuto  chart done

---
+++ src/components/TutoChart.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+
+function TutoChart() {
+    return (
+        <div>
+            <Bar
+                data={{
+                    labels: ['China', 'India', 'USA', 'Indonesia', 'Pakistan'],
+                    datasets:[
+                      {
+                        label:'Population',
+                        data:[ 1440000000, 1370000000, 330160000, 271350000, 221000000 ],
+                        backgroundColor:[
+                          'rgba(255, 99, 132, 0.6)',
+                          'rgba(54, 162, 235, 0.6)',
+                          'rgba(255, 206, 86, 0.6)',
+                          'rgba(75, 192, 192, 0.6)',
+                          'rgba(153, 102, 255, 0.6)'
+                        ]
+                      }
+                    ]
+                }}
+                options={{
+                    title:{
+                    display:true,
+                    text:'The most populated countries in the world',
+                    fontSize:25
+                    },
+                    legend:{
+                    display:false,
+                    position:'bottom'
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+export default TutoChart
