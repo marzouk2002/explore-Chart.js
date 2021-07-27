83250bf6ccceef9b13d7ecf34745650e027dc7aa
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-07-27 19:51:17 +0100

    get methods done

--- src/Classes.js
+++ src/Classes.js
@@ -11,6 +11,46 @@ class DataTime {
     getDays(n) {
         return this.days.splice(0, 6)
     }
+
+    get fiveYears() {
+        return this
+    }
+
+    get oneYear() {
+        return this
+    }
+
+    get sixMonths() {
+        return this
+    }
+
+    get oneMonth() {
+        return this
+    }
+
+    get oneWeek() {
+        return this
+    }
+
+    get oneDay() {
+        return this
+    }
+
+    get fourHours() {
+        return this
+    }
+
+    get oneHour() {
+        return this
+    }
+
+    get halfHour() {
+        return this
+    }
+
+    get  liveScale() {
+        return this
+    }
 }
 
 export default DataTime
